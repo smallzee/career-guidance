cf290df505a55e31a84e9b07383c32759f60dfca
Tech4All <tech4all583@gmail.com>
2021-01-27 09:47:11 +0100

    modify

---
+++ mobile/app/src/main/java/com/app/careerguidance/Developers.java
@@ -0,0 +1,16 @@
+package com.app.careerguidance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Developers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developers);
+        this.setTitle("About Developers");
+
+    }
+}
