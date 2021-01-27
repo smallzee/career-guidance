aa96c85d84b6f60e197ed9ef219f99fdf069d482
Tech4All <tech4all583@gmail.com>
2021-01-27 13:41:34 +0100

    modify

--- mobile/app/src/main/java/com/app/careerguidance/Core.java
+++ mobile/app/src/main/java/com/app/careerguidance/Core.java
@@ -1,6 +1,6 @@
 package com.app.careerguidance;
 
 public class Core {
-    public static final String SITE_URL = "https://projects.veraciouspay.com/clearance/api.php";
+    public static final String SITE_URL = "https://tech4all.com.ng/projects/career-guidance/api.php";
     public static final String IMG_URL = "https://www.federalpolyede.edu.ng/passport/Reg";
 }
